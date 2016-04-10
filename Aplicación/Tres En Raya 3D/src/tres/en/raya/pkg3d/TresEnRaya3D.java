13b3b02a7dbba40eb536483e44576fb92e4c277a
marnavaldez <marnavaldez@gmail.com>
2016-04-09 22:36:20 -0500

    Creación de la carpeta Aplicaciones y el proyecto en NetBeans 8.1

---
+++ Aplicación/Tres En Raya 3D/src/tres/en/raya/pkg3d/TresEnRaya3D.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tres.en.raya.pkg3d;
+
+import tres.en.raya.pkg3d.vista.TableroDelJuego;
+
+/**
+ *
+ * @author MAR
+ */
+public class TresEnRaya3D {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TableroDelJuego juego = new TableroDelJuego();
+        juego.setVisible(true);
+    }
+    
+}
